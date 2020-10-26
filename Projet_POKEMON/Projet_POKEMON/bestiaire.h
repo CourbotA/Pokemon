dfdcee14492ffb327c38c9e0e793452c5403a8c9
CourbotA <72599332+CourbotA@users.noreply.github.com>
2020-10-26 11:17:45 +0100

    Add files via upload

---
+++ Projet_POKEMON/Projet_POKEMON/bestiaire.h
@@ -0,0 +1,8 @@
+#pragma once
+#include"PokemonTypes.h"
+#include"Pokemon.h"
+#include<string>
+
+void initBestiaire(EspecePokemon bestiaire[]);
+void showBestiaire(EspecePokemon bestiaire[]);
+void Tri(EspecePokemon bestiaire[]);
